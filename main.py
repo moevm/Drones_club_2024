9a9187e2be5340a6bf9e34fac24b6716d6011a1f
student <student@student.myguest.virtualbox.org>
2025-04-14 00:07:52 +0300

    feat: add map

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
 from gym_pybullet_drones.utils.Logger import Logger
 from our_env.AutoAviary import AutoAviary
+from our_env.Map import Route
 
 DEFAULT_DRONE = DroneModel('cf2x')
 DEFAULT_GUI = True
@@ -17,13 +18,16 @@ DEFAULT_CONTROL_FREQ_HZ = 48
 
 DEFAULT_OUTPUT_FOLDER = 'my_results'
 DEFAULT_COLAB = True
-DEF_VISION_ATTR = True
+DEFAULT_VISION_ATTR = True
+
+def normalize_angle(angle):
+    return (angle + np.pi) % (2 * np.pi) - np.pi
 
 def run(
         drone=DEFAULT_DRONE,
         gui=DEFAULT_GUI,
         record_video=DEFAULT_RECORD_VIDEO,
-        vision_attributes=DEF_VISION_ATTR,
+        vision_attributes=DEFAULT_VISION_ATTR,
         simulation_freq_hz=DEFAULT_SIMULATION_FREQ_HZ,
         control_freq_hz=DEFAULT_CONTROL_FREQ_HZ,
         output_folder=DEFAULT_OUTPUT_FOLDER,
@@ -31,13 +35,13 @@ def run(
         colab=DEFAULT_COLAB
 ):
     
-    # Начальные параметры для дрона.
     x0, y0, z0 = 0, 0, .1
     INIT_XYZS = np.array([[x0, y0, z0]])
 
     env = AutoAviary(drone_model=drone,
                      num_drones=1,
                      initial_xyzs=INIT_XYZS,
+                     physics=Physics.PYB_GND,
                      neighbourhood_radius=10,
                      pyb_freq=simulation_freq_hz,
                      ctrl_freq=control_freq_hz,
@@ -46,58 +50,85 @@ def run(
                      obstacles=True,
                      vision_attributes=vision_attributes)
 
+    my_route = Route()
     
+    # Добавляем три точки с координатами и углами RPY
+    my_route.add_point([0.0, 0.0, 0.1], [0.0, 0.0, 0.0])   # Стартовая точка
+    my_route.add_point([0.0, 0.0, 2.1], [0.0, 0.0, 1.57])   # Подняться на 2 метра и повернуться на 90 градусов против часовой стрелки
+    my_route.add_point([3.0, 3.0, 2.1], [0.0, 0.0, 1.57])   # Двигаться вдоль оси Y на 3 метра
+
     logger = Logger(logging_freq_hz=control_freq_hz,
                     num_drones=1,
                     output_folder=output_folder,
                     colab=colab)
 
-    
     ctrl = [DSLPIDControl(drone_model=drone)]
    
-   # Начальные значения для управления движением.
     action = np.zeros((1, 4))
-    current_position = INIT_XYZS[0].copy()
-    current_orientation = np.array([0.0, 0.0, 0.0])  # roll-pitch-yaw (r-p-y)
-    height_target_reached = False
-   
+    
     START = time.time()
-
-    i = 0  # Инициализируем счетчик времени
-
-    while True:  # Бесконечный цикл симуляции.
-        #### Step the simulation ###################################
+    i = 0
+    current_orientation = [0.0, 0.0, 0.0]
+    # Добавьте эти параметры в функцию run
+    MOVE_STEP = 1.0  # Шаг перемещения (метры)
+    ROTATE_STEP = 0.01  # Шаг поворота (радианы)
+    new_orientation = [0.0, 0.0, 0.0]
+    # Состояние дрона
+    state = "moving_to_target"  # Начальное состояние
+    while True:
+        
         obs, reward, terminated, truncated, info = env.step(action)
-
-        # Получаем текущее состояние дрона.
         current_position = obs[0][:3]
-        current_orientation = obs[0][3:6]
+        if i: 
+            current_orientation = new_orientation[:]  
 
-        if not height_target_reached:
-            # Двигаемся вверх до высоты 3.
-            if current_position[2] < 3:
-                target_pos = [current_position[0], current_position[1], 3]  # Целевая позиция по Z - 3 метра
+        target_pos = my_route.get_current_point().coordinates.copy()
+        target_orientation = my_route.get_current_point().orientation.copy()
+
+        new_orientation = np.copy(current_orientation)  # Инициализация new_orientation по умолчанию
+
+        if state == "moving_to_target":
+            # Проверяем достижение текущей точки маршрута.
+            if np.linalg.norm(current_position - target_pos) < .1:
+                state = "rotating_to_orientation"  # Переход к следующему состоянию
+
+            # Вычисляем направление к целевой позиции
+            direction = target_pos - current_position
+            distance_to_target = np.linalg.norm(direction)
+
+            if distance_to_target > MOVE_STEP:
+                direction_normalized = direction / distance_to_target
+                new_position = current_position + direction_normalized * MOVE_STEP
             else:
-                height_target_reached = True
+                new_position = target_pos  # Достигли цели
 
-        elif height_target_reached and current_position[2] >= 3:
-            # Двигаемся вперед на 5 метров.
-            if current_position[0] < 5: 
-                target_pos = [5, current_position[1], current_position[2]]  # Целевая позиция по X - 5 метров вперед
-            else:
-                target_pos = [current_position[0], current_position[1], current_position[2]] 
+        elif state == "rotating_to_orientation":
+            # Вычисляем необходимый угол поворота
+            desired_orientation = target_orientation
+            angle_diff = normalize_angle(desired_orientation - current_orientation)
 
-        if env.tag_of_cube:
-            print("&&&&&&&&&&&&&&&&&&&&")
-            # Если дрон видит тег - возвращаемся на координаты (0, 0, 0).
-            target_pos[:] = [x0, y0, z0]
-            height_target_reached = False
+            # Обновляем ориентацию, приближаясь к нужному углу
+            for j in range(len(current_orientation)):
+                if abs(angle_diff[j]) > ROTATE_STEP:
+                    new_orientation[j] = current_orientation[j] + np.sign(angle_diff[j]) * ROTATE_STEP
+                else:
+                    new_orientation[j] = desired_orientation[j]
 
-        # Вычисляем действия с помощью контроллера.
+            # Проверяем достигли ли нужного угла
+            if all(abs(angle_diff) <= ROTATE_STEP):
+                state = "moving_to_next_point"  # Переход к следующему состоянию
+
+        elif state == "moving_to_next_point":
+            my_route.next_point()  # Переход к следующей точке маршрута.
+            state = "moving_to_target"  # Возвращаемся в начальное состояние
+
+        current_orientation = new_orientation[:]
+
+        print(current_orientation, new_orientation)
         action[0], _, _ = ctrl[0].computeControlFromState(control_timestep=env.CTRL_TIMESTEP,
-                                                           state=obs[0],
-                                                           target_pos=target_pos,
-                                                           target_rpy=current_orientation)  
+                                                            state=obs[0],
+                                                            target_pos=new_position,
+                                                            target_rpy=new_orientation)
 
         for j in range(1):
             logger.log(drone=j,
@@ -106,25 +137,16 @@ def run(
 
         env.render()
 
-        #### Sync the simulation ###################################
         if gui:
             sync(i % env.CTRL_FREQ , START , env.CTRL_TIMESTEP)
 
         i += 1
-
-   #### Close the environment #################################
-    env.close()
-
-    if plot:
-        logger.plot()
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='TBD')
     parser.add_argument('--drone',              default=DEFAULT_DRONE,type=DroneModel , help='Drone model', metavar='', choices=list(DroneModel))
     parser.add_argument('--gui',                default=DEFAULT_GUI,type=str2bool , help='Whether to use PyBullet GUI', metavar='')
     parser.add_argument('--record_video',       default=DEFAULT_RECORD_VIDEO,type=str2bool , help='Whether to record a video', metavar='')
-    parser.add_argument('--vision_attributes',   default=DEF_VISION_ATTR,type=str2bool , help='Whether to record a video from drone', metavar='')
+    parser.add_argument('--vision_attributes',   default=DEFAULT_VISION_ATTR,type=str2bool , help='Whether to record a video from drone', metavar='')
     parser.add_argument('--simulation_freq_hz', default=DEFAULT_SIMULATION_FREQ_HZ,type=int , help='Simulation frequency in Hz', metavar='')
     parser.add_argument('--control_freq_hz',     default=DEFAULT_CONTROL_FREQ_HZ,type=int , help='Control frequency in Hz', metavar='')
     parser.add_argument('--output_folder',      default=DEFAULT_OUTPUT_FOLDER,type=str , help='Folder where to save logs', metavar='')
